90d16a82e7d6c0d56c623e34755bb176c7b60b5e
Taha Halim <mohammedtahahalim@proton.me>
2026-05-11 18:14:37 +0100

    implemented the word endpoint, that takes a word; compare it with word of the day and generate a new game state that represent the current state of the game

--- backend/controllers/postWord.controller.ts
+++ backend/controllers/postWord.controller.ts
@@ -8,6 +8,7 @@ import { jwtGenerate } from "../utils/jwt.generate.ts";
 
 export const postWordController = {
   postWord: async (req: Request, res: Response) => {
+    // verifying is a game state has already been initialized and is valid
     const cookies = parse(req.headers.cookie ?? "");
     const { token } = cookies;
     const previousGameState = jwtVerify.verifyToken(token ?? "");
@@ -16,12 +17,14 @@ export const postWordController = {
         .status(400)
         .json({ code: "badRequest", message: "Game has not been initiated" });
 
+    // disallowed request if game has already finished
     if (
       previousGameState.status === "won" ||
       previousGameState.status === "failed"
     )
       return res.status(200).json({ game: previousGameState });
 
+    // sanitizing word in body
     const parsed = wordSchema.safeParse(req.body);
     if (!parsed.success)
       return res.status(400).json({
@@ -29,7 +32,25 @@ export const postWordController = {
         message: "Non valid word in post request",
       });
 
+    // verifying is the word is a valid english word
     const { word } = parsed.data;
+    const isValidWord = await postWordService.isValidWord(word);
+    if (!isValidWord)
+      return res
+        .status(422)
+        .json({ code: "notValid", message: "Not a valid word" });
+
+    const isWordPreviouslySubmitted = !!previousGameState.words.filter(
+      (w) => w.word === word,
+    ).length;
+
+    if (isWordPreviouslySubmitted)
+      return res.status(422).json({
+        code: "previouslySubmitted",
+        message: "This word has been previously submitted",
+      });
+
+    // generating a state of comparing between submitted word and word of the day
     const match = await postWordService.verifyWord(word);
     const { attemptLeft, words, createdAt } = previousGameState;
     const status: GameState["status"] =
--- backend/services/postWord.service.ts
+++ backend/services/postWord.service.ts
@@ -1,22 +1,102 @@
+import type { RowDataPacket } from "mysql2";
 import type { GameState } from "../../shared/types.ts";
 import { client } from "../config/cache.ts";
+import { db } from "../config/db.ts";
+
+type Word = {
+  count: number;
+  idx: Set<number>;
+};
 
 type Case = GameState["words"][number];
 
 export const postWordService = {
-  verifyWord: async (word: string): Promise<Case> => {
+  isValidWord: async function (word: string): Promise<boolean> {
+    const connection = db();
+    const [rows] = await connection.query<RowDataPacket[]>(
+      `SELECT words FROM wordlist WHERE words = ?`,
+      [word],
+    );
+    if (!rows.length) return false;
+    return true;
+  },
+  // retreive word of the day from cache if it exists, otherwise fetch random word from db and then save to cache
+  getWordOfTheDay: async function (): Promise<string> {
     const today = new Date().toDateString();
-    const wordOfTheDay = (await client.get(today)) as string;
-    const wordOfTheDayChars = wordOfTheDay.split("");
+    const word = await client.get(today);
+    // cache hit
+    if (word) return word;
+
+    // generating a new word of the day
+    const connection = db();
+    const [maxRow] = await connection.query<RowDataPacket[]>(
+      `SELECT COUNT(*) as offset FROM wordlist`,
+    );
+    if (!maxRow.length) return "";
+
+    const { offset } = maxRow[0];
+
+    const [rows] = await connection.query<RowDataPacket[]>(
+      `SELECT words FROM wordlist limit 1 offset ?`,
+      [offset],
+    );
+    if (!rows.length) return "";
+    const { words } = rows[0];
+    // saving to cache
+    await client.set(today, words);
+    return words;
+  },
+  // build a map from word of the day, with each char as a key and its count and positions as values
+  buildWordOfTheDayMap: async function (): Promise<Map<string, Word>> {
+    const wordOfTheDay = await this.getWordOfTheDay();
+    const wordOfTheDayChars = new Map<string, Word>();
+    for (const [index, char] of [...wordOfTheDay].entries()) {
+      const { count, idx } = wordOfTheDayChars.get(char) || {
+        count: 0,
+        idx: new Set(),
+      };
+      idx.add(Number(index));
+      wordOfTheDayChars.set(char, {
+        count: count + 1,
+        idx,
+      });
+    }
+    return wordOfTheDayChars;
+  },
+  verifyWord: async function (word: string): Promise<Case> {
+    const wordOfTheDayChars = await this.buildWordOfTheDayMap();
+    // initialize all positions as incorrect
     const res: Case["state"] = Array.from({ length: 5 }, () => "incorrect");
-    word.split("").forEach((char, idx) => {
-      if (char === wordOfTheDayChars[idx]) {
-        res[idx] = "correct";
-        wordOfTheDayChars[idx] = "";
-      } else {
-        if (wordOfTheDayChars.includes(char)) res[idx] = "misplaced";
+
+    // first traverse for correct and in correct positions chars
+    word.split("").forEach((char, index) => {
+      if (wordOfTheDayChars.has(char)) {
+        const { count, idx } = wordOfTheDayChars.get(char)!;
+        if (!!count && idx.has(index)) {
+          res[index] = "correct";
+          idx.delete(index);
+          wordOfTheDayChars.set(char, {
+            count: count - 1,
+            idx,
+          });
+        }
       }
     });
+
+    // second iterations for misplaced char
+    word.split("").forEach((char, index) => {
+      if (wordOfTheDayChars.has(char)) {
+        const { count, idx } = wordOfTheDayChars.get(char)!;
+        if (count) {
+          res[index] = "misplaced";
+          wordOfTheDayChars.set(char, {
+            count: count - 1,
+            idx,
+          });
+        }
+      }
+    });
+
     return { word, state: res };
   },
 };
