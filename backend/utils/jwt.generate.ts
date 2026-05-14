c21be1b36d12c3203fc539bc189ca8888de0d032
Taha Halim <mohammedtahahalim@proton.me>
2026-05-14 10:53:09 +0100

    minor tweaks

--- backend/utils/jwt.generate.ts
+++ backend/utils/jwt.generate.ts
@@ -1,21 +1,21 @@
-import jwt from "jsonwebtoken"
-import "../config/dotenv.ts"
-import type { GameState } from "../../shared/types.ts"
+import jwt from "jsonwebtoken";
+import "../config/dotenv.ts";
+import type { GameState } from "../../shared/types.ts";
 
-const { SECRET_KEY } = process.env
+const { SECRET_KEY } = process.env;
 
-if (!SECRET_KEY) throw new Error('Missing JWT Key')
+if (!SECRET_KEY) throw new Error("Missing JWT Key");
 
 export const jwtGenerate = {
-    generateToken: (payload: GameState): string => {
-        try {
-            const token = jwt.sign(payload, SECRET_KEY, {
-                expiresIn: "1d"
-            })
-            return token
-        } catch (err) {
-            console.log(err)
-            return ""
-        }
+  generateToken: (payload: GameState): string => {
+    try {
+      const token = jwt.sign(payload, SECRET_KEY, {
+        expiresIn: "1d",
+      });
+      return token;
+    } catch (err) {
+      console.log(err);
+      return "";
     }
-}
+  },
+};
