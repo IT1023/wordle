22713e5e4442c4b620c253803624e12cef3b8878
it1023_ <mohamedtahahalim@gmail.com>
2026-09-01 11:46:48 +0100

    tweaks
    ** a11y enhancements

--- frontend/features/menu/Burger.tsx
+++ frontend/features/menu/Burger.tsx
@@ -1,10 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from "react-i18next";
 
 export default function Burger() {
+  const { t } = useTranslation("common");
   return (
-    <div className="w-fit h-full flex items-center cursor-pointer hover:scale-110 transition-all duration-400 text-black dark:text-white">
+    <button
+      aria-label={t("openMenu")}
+      className="w-fit h-full flex items-center cursor-pointer hover:scale-110 transition-all duration-400 text-black dark:text-white"
+    >
       <FontAwesomeIcon icon={faBars} fontSize={22} />
-    </div>
+    </button>
   );
 }
